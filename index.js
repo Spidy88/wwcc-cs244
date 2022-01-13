8afbddac2508ea312e1a319da565109cadd92674
Nick Ferraro <the.nick.ferraro@gmail.com>
2022-01-13 09:00:51 -0800

    Change isLoggedIn to use a user object

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ console.log('Feature B now exists');
 console.log('Feature C1 is ready to go');
 console.log('Feature C2 is ready');
 console.log('Feature D1 reporting for duty');
+console.log('Feature D2 is meh');
 console.log('Goodbye!');
